117ec4fa55cd80b4664fa88323f59b9240edf4f5
NepJNQ <786133176@qq.com>
2021-01-22 16:57:45 +0800

    webpac-vue_2

---
+++ webpack/webpack-vue_2/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <p class="hello">{{ message }}</p>
+    <my-slides></my-slides>
+  </div>
+</template>
+
+<script>
+import MySlides from "./components/MySlides";
+export default {
+  name: 'App',
+  components: { "my-slides": MySlides },
+  data() {
+    return {
+      message: 'hello Webpack! 😎'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hello {
+  color: #000000;
+  text-align: center;
+  font-size: 20px;
+}
+</style>
---
+++ webpack/webpack-vue_2/src/components/MySlides.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="slide-container">
+    <div class="slide-items">
+      <div v-for="item in imgs" :key="item.index" class="slide-item"></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      imgs: [{ name: 'a' }, { name: 'b' }, { name: 'c' }]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+$container-width: 800px;
+$container-height: 450px;
+$imgs-number: 3;
+
+.slide-container {
+  overflow: hidden;
+  width: $container-width;
+  height: $container-height;
+  margin: auto;
+  color: #fff;
+}
+.slide-items {
+  width: $container-width * $imgs-number;
+  animation: marginLeft 10.5s infinite;
+}
+
+.slide-items .slide-item {
+  float: left;
+  width: $container-width;
+  height: $container-height;
+}
+.slide-items .slide-item:nth-child(1) {
+  background: url("../assets/imgs/a.jpg");
+  background-size: $container-width $container-height;
+}
+
+.slide-items .slide-item:nth-child(2) {
+  background: url("../assets/imgs/b.jpg");
+  background-size: $container-width $container-height;
+}
+
+.slide-items .slide-item:nth-child(3) {
+  background: url("../assets/imgs/c.jpg");
+  background-size: $container-width $container-height;
+}
+@keyframes marginLeft {
+  0%,
+  25% {
+    margin-left: 0;
+  }
+  35%,
+  65% {
+    margin-left: -800px;
+  }
+  75%,
+  95% {
+    margin-left: -1600px;
+  }
+
+  100% {
+    margin-left: 0;
+  }
+}
+</style>
